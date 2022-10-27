5f80a8147e28ae025381b24d51388879e2db9529
Isidora <jankov.isidora@gmail.com>
2022-10-27 18:32:57 +0200

    delete pyzdcf import in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../pyzdcf'))
 
-import pyzdcf
-
 # -- Project information -----------------------------------------------------
 
 project = u'pyZDCF'
